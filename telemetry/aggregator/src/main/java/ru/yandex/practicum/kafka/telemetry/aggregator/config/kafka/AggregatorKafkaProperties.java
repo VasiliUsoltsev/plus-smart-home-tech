59a4029977ec1e8a33298a0a6ad491cf7f28484c
Vasili Usoltsev <vasili.n.usoltsev@yandex.com>
2026-09-27 00:21:28 +0700

    feat: добавляю сервер конфигурации

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/config/kafka/AggregatorKafkaProperties.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/telemetry/aggregator/config/kafka/AggregatorKafkaProperties.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "aggregator.kafka")
+@ConfigurationProperties(prefix = "kafka")
 @Getter
 @Setter
 public class AggregatorKafkaProperties {

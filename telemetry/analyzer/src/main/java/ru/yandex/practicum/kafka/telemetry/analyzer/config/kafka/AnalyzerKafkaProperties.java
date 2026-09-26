7d0abd3c9ddd987cbed070e2ae15e64b42dc51c1
Vasili Usoltsev <vasili.n.usoltsev@yandex.com>
2026-09-27 00:33:18 +0700

    Доработал анализатор для взаимодействия с сервером конфигурации

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/kafka/telemetry/analyzer/config/kafka/AnalyzerKafkaProperties.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/kafka/telemetry/analyzer/config/kafka/AnalyzerKafkaProperties.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "analyzer.kafka")
+@ConfigurationProperties(prefix = "kafka")
 @Getter
 @Setter
 public class AnalyzerKafkaProperties {
